d0046b56a814ccd5d9205f3b1fe18b189799426d
Lucas Ferreira <lucasfteo@gmail.com>
2023-04-24 18:41:24 -0300

    directory changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormLoginComponent } from './components/form-login/form-login.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormLoginComponent
+    FormLoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
