ea91c7534ae411c1b0b65d4722ef489d71aaf0d2
Lucas Ferreira <lucasfteo@gmail.com>
2023-05-16 15:52:02 -0300

    user service configuration

--- src/app/components/form-register/form-register.component.ts
+++ src/app/components/form-register/form-register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/user';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-register',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class FormRegisterComponent {
 
+  user: User = {
+    name: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    password: "",
+    gender: ""
+  }
+
+  constructor(
+    private service: UserService,
+    private router: Router
+  ) {}
+
+  createNewUser(){
+    this.service.create(this.user).subscribe(() => {
+      this.router.navigate(['/formLogin'])
+    })
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private readonly API = 'http://127.0.0.1:5000/users'
+
   constructor(private http: HttpClient) { }
+
+  create(user: User): Observable<User> {
+    const url = `${this.API}/${user.id}`
+    return this.http.post<User>(url, user)
+  }
 }
